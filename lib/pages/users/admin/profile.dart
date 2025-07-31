98ce1bb199c2e1e6d66019b025a4d6b41f2c9f66
KarandePrathamesh <karandep703@gmail.com>
2025-07-31 16:31:53 +0530

    created admin profile page

---
+++ lib/pages/users/admin/profile.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class AdminProfile extends StatefulWidget {
+  const AdminProfile({super.key});
+
+  @override
+  State<AdminProfile> createState() => _AdminProfileState();
+}
+
+class _AdminProfileState extends State<AdminProfile> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(child: Text("Admin Profile Page......")),
+    );
+  }
+}
